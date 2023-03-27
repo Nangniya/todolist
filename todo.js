ce8002c4123845ca6b5b64799c9aafbd491ed613
Nayeon <apinksky00@gmail.com>
2023-03-27 20:58:00 +0900

    모두삭제 스타일링 및 todovalue 초기화 안되는거 고쳐야함

--- todo.js
+++ todo.js
@@ -1,4 +1,5 @@
 document.querySelector('.submit').addEventListener('click', addToDo);
+document.querySelector('.clear').addEventListener('click', clearAll);
 
 
 function addToDo(e){ //할 일 추가
@@ -6,13 +7,16 @@ function addToDo(e){ //할 일 추가
     let toDoValue = document.querySelector('input').value;
     if(toDoValue !== ''){
         addTask(toDoValue);
-        toDoValue = ''; //입력창 비워주기
+        toDoValue = ''; //입력창 초기화
     }
 }
-
 function addTask(value){
     let ul = document.querySelector('ul');
     let li = document.createElement('li');
     li.innerHTML = `${value}`
     ul.appendChild(li);
+}
+
+function clearAll(){ //모두 삭제
+    document.querySelector('ul').innerHTML = '';
 }
