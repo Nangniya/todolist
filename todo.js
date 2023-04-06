c44ba1ece8b8cbca3c56ea76a1aad0896371967c
Nayeon <apinksky00@gmail.com>
2023-04-06 21:55:53 +0900

    로컬스토리지 기능 추가

--- todo.js
+++ todo.js
@@ -1,5 +1,17 @@
 document.querySelector('.submit').addEventListener('click', addToDo);
 document.querySelector('.clear').addEventListener('click', clearAll);
+document.addEventListener('DOMContentLoaded', () => { //로드될 때 로컬스토리지에서 가져오기
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (tasks) {
+        tasks.forEach(task => addTask(task));
+    }
+    document.querySelectorAll('.trash').forEach(item => {
+        item.addEventListener('click', deleteList);
+    });
+    document.querySelectorAll('.check').forEach(item => {
+        item.addEventListener('click', checkToggle);
+    });
+});
 
 function addToDo(e){ //할 일 추가
     e.preventDefault();
@@ -7,6 +19,7 @@ function addToDo(e){ //할 일 추가
     if(toDoValue !== ''){
         addTask(toDoValue);
         document.querySelector('input').value = ''; //입력창 초기화
+        saveToLocalStorage(); //로컬스토리지 저장
     }
     document.querySelectorAll('.trash').forEach(item => { //삭제 이벤트 부여
         item.addEventListener('click', deleteList);
@@ -15,7 +28,13 @@ function addToDo(e){ //할 일 추가
         item.addEventListener('click', checkToggle);
     });
 }
-function addTask(value){
+function saveToLocalStorage() { //로컬스토리지 저장
+    const tasks = document.querySelectorAll('li span');
+    const tasksArr = [];
+    tasks.forEach(task => tasksArr.push(task.textContent));
+    localStorage.setItem('tasks', JSON.stringify(tasksArr));
+}
+function addTask(value){ //리스트 추가
     let ul = document.querySelector('ul');
     let li = document.createElement('li');
     li.setAttribute('data-count','0');
@@ -28,6 +47,7 @@ function clearAll(){ //모두 삭제
     const result = confirm('모두 삭제하시겠습니까?')
     if(result){
         document.querySelector('ul').innerHTML = '';
+        localStorage.removeItem('tasks'); //로컬스토리지에서 모두 삭제
     }
 }
 
@@ -36,6 +56,10 @@ function deleteList(e) { //개별 삭제
         let liParent = targetLi.parentNode;
         const result = confirm(`${targetLi.textContent}를 삭제하시겠습니까?`)
         if(result){
+            const spanText = targetLi.querySelector('span').textContent;
+            const tasks = JSON.parse(localStorage.getItem('tasks'));
+            const updatedTasks = tasks.filter(task => task !== spanText);
+            localStorage.setItem('tasks', JSON.stringify(updatedTasks));
             liParent.removeChild(targetLi);
         }
 }
