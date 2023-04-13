145e25b0abad0f09bf58d52312718a0f77efef38
Nayeon <apinksky00@gmail.com>
2023-04-13 11:33:05 +0900

    server

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-// const express = require('express')
-// const app = express()
-// const MongoClient = require('mongodb').MongoClient
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+app.set('view engine', 'ejs');
 
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-
-// MongoClient.connect('mongodb+srv://apinksky00:<password>@cluster0.ucdd5er.mongodb.net/?retryWrites=true&w=majority', function(에러, client){
-//   if (에러) return console.log(에러)
-//   app.listen(8080, function() {
-//     console.log('listening on 8080')
-//   })
-// })
-
-//여기 이하는 쓸데없는 app.get 이런 코드들
+let db;
+MongoClient.connect('mongodb+srv://apinksky00:eocndtkffo07@cluster0.ucdd5er.mongodb.net/?retryWrites=true&w=majority', function(에러, client){
+   if (에러) return console.log(에러)
+   db = client.db('mytodo');
+   app.listen(8080, function() {
+    console.log('listening on 8080')
+   })
+ })
+app.get('/', function(요청, 응답){
+    응답.render('todo.ejs');
+}) 
+app.post('/add', function(요청, 응답){
+    db.collection('todolist').insertOne({ 할일 : 요청.body.task }, function(에러, 결과){
+        console.log(결과);
+    })
+    응답.redirect('/');
+})
